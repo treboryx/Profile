0c841c45eb049957baee1fae7c36dab4dc44e41e
Robert <treboryx@gmail.com>
2019-10-04 00:07:03 +0200

    Initial dist subtree commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 
 module.exports = {
-    outputDir: path.resolve(__dirname, "./docs")
+    // outputDir: path.resolve(__dirname, "./docs"),
+    publicPath: "Profile"
 };
